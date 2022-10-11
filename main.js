ec6b0a73853e2ad3c86ee9190e7745c78e88d096
ayush101-py <ayush.agrawal.101.cse@gmail.com>
2022-10-11 13:18:09 +0100

    Some Fix for WebView2 + some experiments

--- main.js
+++ main.js
@@ -1,109 +1,140 @@
-let innerPercent = 50
-let innerRadius = 270
-let barPercent = 50
-let maxLength = 1080 / 2 - innerRadius
-let barWidth = 75
-let startHue = 0
-let endHue = 180
-let saturation = 50
-let lightness = 50
-let compensation = 50
-let glow = 10
-let innerMovement = 25
+let innerPercent = 50;
+let innerRadius = 270;
+let barPercent = 50;
+let maxLength = 1080 / 2 - innerRadius;
+let barWidth = 75;
+let startHue = 0;
+let endHue = 180;
+let saturation = 50;
+let lightness = 50;
+let compensation = 50;
+let glow = 10;
+let innerMovement = 25;
 
-let showStars = true
-let starColor = "#FFFFFF"
-let starOpacity = 50
-let starGlow = 15
+let showStars = true;
+let starColor = "#FFFFFF";
+let starOpacity = 50;
+let starGlow = 15;
 
-let xPercent = 50
-let yPercent = 50
-let movementSpeed = 50
-let movementRadius = 15
-let roundedBars = false
+let xPercent = 50;
+let yPercent = 50;
+let movementSpeed = 50;
+let movementRadius = 15;
+let roundedBars = false;
 
-let shadowX = 8
-let shadowY = 8
-let shadowBlur = 10
-let shadowOpacity = 70
+let shadowX = 8;
+let shadowY = 8;
+let shadowBlur = 10;
+let shadowOpacity = 70;
 
-const debug = document.getElementById("debug")
-const middle = document.getElementById("middle")
-const canvas = document.getElementById("canvas")
-const visualizer = document.getElementById("visualizer")
-const starfield = document.getElementById("starfield")
+let middleEnabled = false;
+
+let starStarted = false;
+let starInterval;
+
+const debug = document.getElementById("debug");
+const middle = document.getElementById("middle");
+const canvas = document.getElementById("canvas");
+const visualizer = document.getElementById("visualizer");
+const starfield = document.getElementById("starfield");
 // Fullscreen canvas
-canvas.width = document.body.clientWidth
-canvas.height = document.body.clientHeight
-let axis = 0
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+let axis = 0;
 
-const ctx = canvas.getContext("2d")
+let count = 20;
 
-function livelyAudioListener(audioArray) {
+const ctx = canvas.getContext("2d");
+
+function livelyAudioListener(audioArray)  {
+  /*while(count > 0){
+    console.log(audioArray);
+    count--;
+  }*/
   // Set overall level
-  average = audioArray.reduce((acc, val) => acc + val) / audioArray.length
-  star_speed = average * 32
+  average = audioArray.reduce((acc, val) => acc + val) / audioArray.length;
+  star_speed = average * 32;
   // Remove duplicate frequencies
-  let audio = audioArray.filter((elem, idx, arr) => arr[idx - 1] !== elem)
+  let audio = audioArray.filter((elem, idx, arr) => arr[idx - 1] !== elem);
   // Compensate for overamplified bass
   audio = audio.map((elem, idx, arr) => {
-    return elem * (idx / arr.length + (100 - compensation + 50) / 100)
-  })
+    return elem * (idx / arr.length + (100 - compensation + 50) / 100);
+  });
   // Mirror on vertical axis
-  audio = audio.slice().reverse().concat(audio)
+  audio = audio.slice().reverse().concat(audio);
 
-  let innerRadius = (ctx.canvas.height / 2) * (innerPercent / 100)
-  let maxLength = (ctx.canvas.height / 2 - innerRadius) * (barPercent / 100)
+  let innerRadius = (ctx.canvas.height / 2) * (innerPercent / 100);
+  let maxLength = (ctx.canvas.height / 2 - innerRadius) * (barPercent / 100);
   // Bar length, val = 100 -> reach border of screen
 
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-  ctx.lineWidth = ((barWidth / 100) * (2 * innerRadius * Math.PI)) / audio.length
-  ctx.lineCap = roundedBars ? "round" : "butt"
-  ctx.shadowBlur = glow
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.lineWidth = ((barWidth / 100) * (2 * innerRadius * Math.PI)) / audio.length;
+  ctx.lineCap = roundedBars ? "round" : "butt";
+  ctx.shadowBlur = glow;
   // Visualizer location
-  let xPos = ctx.canvas.width * (xPercent / 100)
-  xPos += (noise(axis) - 0.5) * ((innerRadius * movementRadius) / 100)
-  let yPos = ctx.canvas.height * (yPercent / 100)
-  yPos += (noise(0, axis) - 0.5) * ((innerRadius * movementRadius) / 100)
-  axis += average * (movementSpeed / 100)
+  let xPos = ctx.canvas.width * (xPercent / 100);
+  xPos += (noise(axis) - 0.5) * ((innerRadius * movementRadius) / 100);
+  let yPos = ctx.canvas.height * (yPercent / 100);
+  yPos += (noise(0, axis) - 0.5) * ((innerRadius * movementRadius) / 100);
+  axis += average * (movementSpeed / 100);
   // Logo center image location
-  if (middle.style.display != "none") {
-    middle.style.left = `${xPos}px`
-    middle.style.top = `${yPos}px`
+  if (middleEnabled) {
+    middle.style.left = `${xPos}px`;
+    middle.style.top = `${yPos}px`;
   }
 
   // Visualizer drop shadow
   visualizer.style.filter = `drop-shadow(${shadowX}px ${shadowY}px ${shadowBlur}px rgba(0,0,0,${
-    shadowOpacity / 100
-  }))`
+      shadowOpacity / 100
+  }))`;
 
   if (audio.length > 2) {
+
+    if(middleEnabled)
+    {
+      middle.style.display = "block";
+    }
+    if(!starStarted)
+    {
+      starInterval = setInterval(anim, 16);
+      starStarted = true;
+    }
+
     // Draw each bar
     audio.map((val, index, arr) => {
-      let ratio = (index + 0.5) / arr.length
-      let halfRatio = Math.abs(((index / (arr.length / 2)) % (arr.length / 2)) - 1)
-      ctx.beginPath()
-      ctx.translate(xPos, yPos)
+      let ratio = (index + 0.5) / arr.length;
+      let halfRatio = Math.abs(((index / (arr.length / 2)) % (arr.length / 2)) - 1);
+      ctx.beginPath();
+      ctx.translate(xPos, yPos);
       // Rotate each bar
-      ctx.rotate(2 * Math.PI * ratio)
+      ctx.rotate(2 * Math.PI * ratio + Math.PI); // Rotate 180 Degrees for base on bottom and higher freq on top
 
       // Color from given hue, saturation, lightness
       let color = `hsl(${
-        ((endHue - startHue) * halfRatio + startHue) % 360
-      }, ${saturation}%, ${lightness}%)`
-      ctx.strokeStyle = color
-      ctx.shadowColor = color
+          ((endHue - startHue) * halfRatio + startHue + 90 + (Math.pow(1,index) * 90)) % 360 // Experiment
+      }, ${saturation}%, ${lightness}%)`;
+      ctx.strokeStyle = color;
+      ctx.shadowColor = color;
 
-      let innerOffset = average * maxLength * (innerMovement / 100)
-      // Move to approparite height
-      ctx.moveTo(0, innerRadius + innerOffset)
+      let innerOffset = average * maxLength * (innerMovement / 100);
+      // Move to appropriate height
+      ctx.moveTo(0, innerRadius + innerOffset);
       // Draw line outwards from origin
-      ctx.lineTo(0, val * maxLength + innerRadius + innerOffset)
+      ctx.lineTo(0, val * maxLength + innerRadius + innerOffset);
       // Apply stroke
-      ctx.stroke()
+      ctx.stroke();
       // Reset current transformation matrix to the identity matrix
-      ctx.setTransform(1, 0, 0, 1, 0, 0)
-    })
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+    });
+  }
+  else
+  {
+    middle.style.display = "none";
+    if(!starStarted)
+    {
+      clearInterval(starInterval);
+      starStarted = false;
+    }
   }
 }
 
@@ -111,103 +142,104 @@ function livelyPropertyListener(name, val) {
   // debug.textContent += `Name: ${name} Val: ${val}`
   switch (name) {
     case "innerRadius":
-      innerPercent = val
+      innerPercent = val;
       // Logo image width & height
-      let rad = (ctx.canvas.height / 2) * (val / 100)
-      middle.style.width = `${rad * 2}px`
-      middle.style.height = `${rad * 2}px`
-      break
+      let rad = (ctx.canvas.height / 2) * (val / 100);
+      middle.style.width = `${rad * 2}px`;
+      middle.style.height = `${rad * 2}px`;
+      break;
     case "middleImageScale":
-      middle.style.transform = `translate(-50%, -50%) scale(${val / 100})`
-      break
+      middle.style.transform = `translate(-50%, -50%) scale(${val / 100})`;
+      break;
     case "maxLength":
-      barPercent = val
-      break
+      barPercent = val;
+      break;
     case "bgImage":
-      document.body.style.backgroundImage = `url(/${val.replace(/\\/g, "/")})`
-      break
+      document.body.style.backgroundImage = "url(" + val.replace('\\', '/') + ")";
+      break;
     case "bgBlur":
-      document.body.style.backdropFilter = `blur(${Math.round(val)}px)`
-      break
+      document.body.style.backdropFilter = `blur(${Math.round(val)}px)`;
+      break;
     case "useMiddleImage":
-      middle.style.display = val ? "block" : "none"
-      break
+      //middle.style.display = val ? "block" : "none";
+      middleEnabled = true;
+      break;
     case "middleImage":
-      middle.src = `/${val.replace(/\\/g, "/")}`
-      break
+      middle.style.content = "url(" + val.replace('\\', '/') + ")";
+      break;
     case "barWidth":
-      barWidth = val
-      break
+      barWidth = val;
+      break;
     case "startHue":
-      startHue = val
-      break
+      startHue = val;
+      break;
     case "endHue":
-      endHue = val
-      break
+      endHue = val;
+      break;
     case "saturation":
-      saturation = val
-      break
+      saturation = val;
+      break;
     case "lightness":
-      lightness = val
-      break
+      lightness = val;
+      break;
     case "barCompensation":
-      compensation = val
-      break
+      compensation = val;
+      break;
     case "barGlow":
-      glow = val
-      break
+      glow = val;
+      break;
     case "innerMovement":
-      innerMovement = val
-      break
+      innerMovement = val;
+      break;
     case "showStars":
-      showStars = val
-      break
+      showStars = val;
+      break;
     case "starColor":
-      starColor = val
-      break
+      starColor = val;
+      break;
     case "starOpacity":
-      starOpacity = val
-      break
+      starOpacity = val;
+      break;
     case "starGlow":
-      starGlow = val
-      break
+      starGlow = val;
+      break;
     case "starBlur":
-      starfield.style.filter = `blur(${val / 2}px)`
-      break
+      starfield.style.filter = `blur(${val / 2}px)`;
+      break;
     case "xPercent":
-      xPercent = val
-      break
+      xPercent = val;
+      break;
     case "yPercent":
-      yPercent = val
-      break
+      yPercent = val;
+      break;
     case "shakeSpeed":
-      movementSpeed = val
-      break
+      movementSpeed = val;
+      break;
     case "shakeRadius":
-      movementRadius = val
-      break
+      movementRadius = val;
+      break;
     case "roundedBars":
-      roundedBars = val
-      break
+      roundedBars = val;
+      break;
     case "shadowX":
-      shadowX = val
-      break
+      shadowX = val;
+      break;
     case "shadowY":
-      shadowY = val
-      break
+      shadowY = val;
+      break;
     case "shadowBlur":
-      shadowBlur = val
-      break
+      shadowBlur = val;
+      break;
     case "shadowOpacity":
-      shadowOpacity = val
-      break
+      shadowOpacity = val;
+      break;
 
     default:
       // console.error(`Unknown customization option: ${name}`)
-      break
+      break;
   }
 }
-
+/*
 document.addEventListener("DOMContentLoaded", (event) => {
-  livelyAudioListener([0])
-})
+  livelyAudioListener([0]);
+});*/
